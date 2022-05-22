9bbea108dfedf2fe519b26d88f1dba3ad23e3e8f
cccaaannn <can.kurt.aa@gmail.com>
2022-05-22 21:52:58 +0300

    account delete form

---
+++ components/AccountDeleteConfirmDialog.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import { useState } from 'react';
+
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { AuthUtils } from '../utils/auth-utils';
+
+
+interface AccountDeleteConfirmDialogProps {
+    open: any,
+    setOpen: any,
+    onConfirm: any
+}
+
+export default function AccountDeleteConfirmDialog({ open, setOpen, onConfirm }: AccountDeleteConfirmDialogProps) {
+
+    const [enteredText, setEnteredText] = useState("");
+    const textToMatch = AuthUtils.isLoggedIn() ? AuthUtils.getTokenPayload().username : "";
+
+    const onMatch = () => {
+        if (isMatching()) {
+            onConfirm();
+            setOpen(false);
+        }
+    }
+
+    const isMatching = () => {
+        if (enteredText == textToMatch) {
+            return true;
+        }
+        return false;
+    }
+    
+    return (
+        <div>
+            <Dialog open={open} onClose={() => setOpen(false)}>
+                <DialogTitle>{"Delete your account?"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText gutterBottom>
+                        {"Are you sure you want to delete your account, all of your posts and comments also WILL be deleted."}
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="dialog-text-field"
+                        label={`Please type '${textToMatch}' to delete your account`}
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        onChange={(e) => setEnteredText(e.target.value)}
+                        error={!isMatching()}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpen(false)}>Cancel</Button>
+                    <Button onClick={() => onMatch()} disabled={!isMatching()} >Delete</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
---
+++ components/forms/AccountDeleteForm.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+
+import { Button, Grid, Typography } from "@mui/material";
+
+import { LocalStorageKeys } from "../../types/enums/local-storage-keys";
+import { AuthUtils } from "../../utils/auth-utils";
+import AccountDeleteConfirmDialog from "../AccountDeleteConfirmDialog";
+import { Storage } from '../../utils/storage';
+import { ApiUtils } from '../../utils/api-utils';
+
+
+const AccountDeleteForm = () => {
+
+    const [open, setOpen] = useState(false);
+
+    const onDeleteAccount = async () => {
+        const token = Storage.get(LocalStorageKeys.TOKEN);
+
+        const res = await fetch(`${ApiUtils.getApiUrl()}/users/purge/${AuthUtils.getTokenPayload().id}`, {
+            method: "delete",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+        });
+        console.log(res);
+        
+        const jsonData: any = await res.json();
+        console.log(jsonData);
+
+        AuthUtils.logout();
+        window.location = "/home" as any;
+    }
+
+    return (
+        <>
+            <AccountDeleteConfirmDialog
+                open={open}
+                setOpen={setOpen}
+                onConfirm={() => onDeleteAccount()}
+            />
+            <Grid container spacing={0} sx={{ mt: 4, mb: 4 }} >
+                <Grid item xs={6}>
+                    <Grid item xs={12}>
+                        <Typography gutterBottom variant="h4" component="div">
+                            Delete account
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography gutterBottom variant="subtitle1" component="p">
+                            Permanently delete your account with all of your posts and comments.
+                        </Typography>
+                    </Grid>
+                </Grid>
+                <Grid item xs={6}>
+                    <Grid item xs={12}>
+                        <br />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button
+                            variant="contained"
+                            color="error"
+                            onClick={() => setOpen(true)}
+                            sx={{ float: "right" }}
+                        >
+                            Delete account
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </>
+    );
+}
+
+export default AccountDeleteForm

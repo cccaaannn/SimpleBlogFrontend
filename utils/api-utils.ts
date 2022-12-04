3cd92ad7136415f646339abc8a00ac2afb6e16b9
cccaaannn <can.kurt.aa@gmail.com>
2022-12-04 21:37:15 +0300

    api url changed

--- utils/api-utils.ts
+++ utils/api-utils.ts
@@ -1,7 +1,7 @@
 export namespace ApiUtils {
 
     const dev = process.env.NODE_ENV !== 'production'
-    const serverUrl = dev ? 'http://localhost:3000' : 'https://simple-blog-backend-1.herokuapp.com'
+    const serverUrl = dev ? 'http://localhost:3000' : 'https://api.simpleblog.projects.kurtcan.com'
 
     export function getServerUrl(): string {
         return serverUrl;
